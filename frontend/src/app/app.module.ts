8724f5ddb6825b2b3e87313f566f7abff4068bba
Wittawat Fuangfu <wittawat335@gmail.com>
2023-09-27 20:12:17 +0700

    frontend : create authen components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { SharedModule } from './shared/shared/shared.module';
+import { RegisterComponent } from './components/authen/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LayoutComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
